a17775d07c841b0021c617e323d9138daf365086
soham.patel <soham.patel@afourtech.com>
2022-05-06 06:54:45 +0530

    commit

--- FirstProjectToPOC/Test Suites/Demo/DemoTestSuite1.ts
+++ FirstProjectToPOC/Test Suites/Demo/DemoTestSuite1.ts
@@ -12,10 +12,24 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>0ccc62d0-2842-4a85-9fe2-ae6e2bd3a34c</testSuiteGuid>
    <testCaseLink>
-      <guid>01ca81d2-7af9-4706-8bce-0d39bf304b07</guid>
+      <guid>d55b52de-9735-4e23-9667-c5e838c0827a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Demo/ManualModeDemo</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8995ccfa-357c-4dc9-81c5-02581a711224</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/RecordModeDemo</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c4ddccef-84ad-40a4-8360-c80dd5761223</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/ScriptModeDemo</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
